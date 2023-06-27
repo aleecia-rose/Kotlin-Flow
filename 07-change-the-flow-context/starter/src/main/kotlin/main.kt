cde1c15f05802f9b8d1e79aed3ea83de3e89c1ea
Aleecia Everett <aleeciarose@gmail.com>
2023-06-26 22:29:08 -0400

    Completed Exercise for Section 7
    Completed Exercise for Section 7

--- 07-change-the-flow-context/starter/src/main/kotlin/main.kt
+++ 07-change-the-flow-context/starter/src/main/kotlin/main.kt
@@ -32,9 +32,29 @@
  * THE SOFTWARE.
  */
 
-import kotlinx.coroutines.runBlocking
+ import kotlinx.coroutines.Dispatchers
+ import kotlinx.coroutines.delay
+ import kotlinx.coroutines.flow.*
+ import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
+
+    fun deulOfTheFates(): Flow<ForceUser> = flow {
+        for (forceUser in forceUsers) {
+            delay(DELAY)
+            log("Battling ${forceUser.name}")
+            emit(forceUser)
+        }
+    }.transform { forceUser ->
+        if (forceUser is Sith) {
+            forceUser.name = "Darth ${forceUser.name}"
+        }
+        emit(forceUser)
+    }.flowOn(Dispatchers.Default)
+
+    deulOfTheFates().collect {
+        log("Battled ${it.name}")
+    }
 
 }
 
