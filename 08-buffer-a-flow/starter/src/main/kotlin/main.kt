4084b1c1c8abdd1fc47b99de3ec97eb1b8d6db66
Aleecia Everett <aleeciarose@gmail.com>
2023-06-26 23:03:31 -0400

    Completed Exercises for Section 8
    Completed Exercises for Section 8

--- 08-buffer-a-flow/starter/src/main/kotlin/main.kt
+++ 08-buffer-a-flow/starter/src/main/kotlin/main.kt
@@ -32,9 +32,70 @@
  * THE SOFTWARE.
  */
 
-import kotlinx.coroutines.runBlocking
+ import kotlinx.coroutines.delay
+ import kotlinx.coroutines.flow.*
+ import kotlinx.coroutines.runBlocking
+ import kotlin.system.measureTimeMillis
+
+fun jediTrainees(): Flow<ForceUser> = forceUser.asFlow()
+    .transform { forceUser -> 
+        if (forceUser is Padawan) {
+            delay(DELAY) // Jedi Knight training
+            emit(forceUser)
+        }
+    }
 
 fun main() = runBlocking {
+
+    var time = measureTimeMillis {
+        jediTrainees()
+            .collect { jedi ->
+                delay(3 * DELAY) // Jedi Master training
+                log("Jedi ${jedi.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
+
+
+    exampleOf("buffer")
+
+    time = measureTimeMillis {
+        jediTrainees()
+            .buffer()
+            .collect { jedi ->
+                delay(3 * DELAY) // Jedi Master training
+                log("Jedi ${jedi.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
+
+    exampleOf("conflate")
+
+    time = measureTimeMillis {
+        jediTrainees()
+            .conflate()
+            .collect { jedi ->
+                delay(3 * DELAY) // Jedi Master training
+                log("Jedi ${jedi.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
+
+    exampleOf("collectLatest")
+
+    time = measureTimeMillis {
+        jediTrainees()
+            .collectLastest { jedi ->
+                log("Jedi Master training for ${jedi.name}")
+                delay(3 * DELAY) // Jedi Master training
+                log("Jedi ${jedi.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
 
 }
 
